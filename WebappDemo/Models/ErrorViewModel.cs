e06d5e93f83ff7c4c1d1fc5da22fd1debbe9d193
sivakumar999 <sivakumar59498@gmail.com>
2023-09-20 12:02:54 +0530

    Done

---
+++ WebappDemo/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace WebappDemo.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
